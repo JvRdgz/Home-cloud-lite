7eeee10ad1c098b17e453da31dbe7acc55d54438
JvRdgz <javier.rodriguezjr97@gmail.com>
2021-05-09 12:49:42 +0200

    Eliminadas credenciales mongo

--- app/init_services.sh
+++ app/init_services.sh
@@ -8,7 +8,7 @@ npm i
 
 # Ejecucion del servicio de mongodb de nuestra base de datos
 # PARA CONECTAR CON EL CLUSTER USANDO EL MONGO SHELL. (DESDE EL TERMINAL) ===> COMANDO PARA EL TERMINAL:
-# mongo "mongodb+srv://mysky.ragmu.mongodb.net/mysky" --username usuario-JR:elpepe14
+mongo "mongodb+srv://mysky.ragmu.mongodb.net/mysky" --username usuario-JR:elpepe14
 
 # Ejecución del software
 # node server.js
